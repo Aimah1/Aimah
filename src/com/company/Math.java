e33b8307ec6cf5d04279be67842797c6251a63cb
Aimah <aimahdurrani@hotmail.com>
2019-12-03 11:06:11 +0100

    Added add method to the Math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,4 +1,19 @@
 package com.company;
+import java.util.Scanner;
+
+import org.w3c.dom.ls.LSOutput;
 
 public class Math {
+    public static void main(String[] args) {
+       
+
+
+
+
+}
+
+public int add (int first,int second){
+    return first+second;
+
+}
 }
